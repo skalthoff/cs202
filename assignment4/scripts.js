ff72cec3bbae15998e3d54ba9699158402a162bb
Soren Althoff <sorenalthoff@gmail.com>
2023-04-20 08:31:30 -0700

    doneeeee

--- assignment4/scripts.js
+++ assignment4/scripts.js
@@ -1,59 +1,60 @@
 $(document).ready(function () {
     const canvas = document.getElementById('tetrominoes-background');
     const ctx = canvas.getContext('2d');
+
     const themes = [
         {
-            name: 'Blue / Cyan',
-            description: 'Very crisp colors and reminiscent of Mega Man.',
-            colors: ['#002D69', '#00FFFF'],
+          name: 'Blue / Cyan',
+          description: 'This palette features crisp and refreshing colors that evoke a sense of playfulness and energy, reminiscent of the classic game Mega Man.',
+          colors: ['#002D69', '#00FFFF'],
         },
         {
-            name: 'Forest / Chartreuse',
-            description: 'A nice combo, very fresh and comforting.',
-            colors: ['#128712', '#62D962'],
+          name: 'Forest / Chartreuse',
+          description: 'A natural and calming color combination that brings a sense of freshness and comfort, perfect for a relaxing gaming experience.',
+          colors: ['#128712', '#62D962'],
         },
         {
-            name: 'Magenta / Pink',
-            description: 'This one always makes me think of cotton candy.',
-            colors: ['#FF00FF', '#FFA8FF'],
+          name: 'Magenta / Pink',
+          description: 'A playful and fun palette that reminds us of sweet treats like cotton candy. It\'s a great choice for those who want to add a touch of whimsy to their gameplay.',
+          colors: ['#FF00FF', '#FFA8FF'],
         },
         {
-            name: 'Blue / Green',
-            description: 'Reminds me of the old Seattle Seahawks logo.',
-            colors: ['#1B4C1B', '#4CFF4C'],
+          name: 'Blue / Green',
+          description: 'This palette brings to mind the iconic Seattle Seahawks logo, featuring bold and vibrant colors that are sure to make a statement on the game board.',
+          colors: ['#1B4C1B', '#4CFF4C'],
         },
         {
-            name: 'Red / Seafoam',
-            description: 'Christmasy colors, pastel but just as clashing. Ugly.',
-            colors: ['#9C3838', '#9CFF9C'],
+          name: 'Red / Seafoam',
+          description: 'While this palette may be reminiscent of Christmas, the soft pastel colors create a unique and understated combination, perfect for those who prefer a more subdued look.',
+          colors: ['#9C3838', '#9CFF9C'],
         },
         {
-            name: 'Seafoam / Cornflower',
-            description: 'I looks nice together if you like boring pastels lacking contrast.',
-            colors: ['#00A800', '#0070EC'],
+          name: 'Seafoam / Cornflower',
+          description: 'These pastel colors may be considered "boring" by some, but the subtle contrast of seafoam and cornflower creates a soothing and calming effect on the eyes.',
+          colors: ['#00A800', '#0070EC'],
         },
         {
-            name: 'Red / Gray',
-            description: 'This one reminds me of lava and ash. I\'m always happy to see this palette.',
-            colors: ['#FF0000', '#A0A0A0'],
+          name: 'Red / Gray',
+          description: 'A fiery and dramatic palette that evokes images of lava and ash. The contrast between the bright red and cool gray makes for an exciting and dynamic gameplay experience.',
+          colors: ['#FF0000', '#A0A0A0'],
         },
         {
-            name: 'Purple / Burgundy',
-            description: 'Nice combo, and the only double-dark set.',
-            colors: ['#7B30C7', '#810000'],
+          name: 'Purple / Burgundy',
+          description: 'This palette features a sophisticated and bold color combination that is sure to make a statement. It\'s the only double-dark set, making it a unique and elegant option.',
+          colors: ['#7B30C7', '#810000'],
         },
         {
-            name: 'Blue / Red',
-            description: 'Makes me think of the American flag, except the colors aren\'t dark enough.',
-            colors: ['#0000FF', '#FF0000'],
+          name: 'Blue / Red',
+          description: 'This palette combines the bold and striking colors of blue and red to create a dynamic and energetic atmosphere on the game board. The contrast between the two colors is sure to keep players engaged and focused, making it a great choice for those who want a more intense gaming experience.',
+          colors: ['#0000FF', '#FF0000'],
         },
         {
-            name: 'Red / Orange',
-            description: 'Yuck. It makes me think of clowns or fast food logos.',
-            colors: ['#FF0000', '#FFA000'],
+          name: 'Red / Orange',
+          description: 'Although this palette may not be everyone\'s cup of tea, the bold and bright colors can add a touch of energy and excitement to the game board. It\'s a great choice for those who want to stand out from the crowd.',
+          colors: ['#FF0000', '#FFA000'],
         },
-    ];
-    
+      ];
+      
 
     let currentTheme = 0;
 
