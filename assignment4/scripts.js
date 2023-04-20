ea95f5dc0b8fdb65057cadf1092d01a7ffd7e735
Soren Althoff <sorenalthoff@gmail.com>
2023-04-20 07:52:27 -0700

    added tetris assignment 4

--- assignment4/scripts.js
+++ assignment4/scripts.js
@@ -1,47 +1,143 @@
-// scripts.js
-$(document).ready(function() {
-    $("#switch").on("click", function() {
-        $("#nature").fadeOut("slow", function() {
-            $("#technology").fadeIn("slow");
-        });
-    });
-
-    $("#switch-back").on("click", function() {
-        $("#technology").fadeOut("slow", function() {
-            $("#nature").fadeIn("slow");
-        });
-    });
-});
 $(document).ready(function () {
+    const canvas = document.getElementById('tetrominoes-background');
+    const ctx = canvas.getContext('2d');
     const themes = [
         {
-            title: "Theme 1",
-            description: "Short description for Theme 1.",
+            name: 'Blue / Cyan',
+            description: 'Very crisp colors and reminiscent of Mega Man.',
+            colors: ['#002D69', '#00FFFF'],
         },
         {
-            title: "Theme 2",
-            description: "Short description for Theme 2.",
+            name: 'Forest / Chartreuse',
+            description: 'A nice combo, very fresh and comforting.',
+            colors: ['#128712', '#62D962'],
         },
-        // Add more themes here
+        {
+            name: 'Magenta / Pink',
+            description: 'This one always makes me think of cotton candy.',
+            colors: ['#FF00FF', '#FFA8FF'],
+        },
+        {
+            name: 'Blue / Green',
+            description: 'Reminds me of the old Seattle Seahawks logo.',
+            colors: ['#1B4C1B', '#4CFF4C'],
+        },
+        {
+            name: 'Red / Seafoam',
+            description: 'Christmasy colors, pastel but just as clashing. Ugly.',
+            colors: ['#9C3838', '#9CFF9C'],
+        },
+        {
+            name: 'Seafoam / Cornflower',
+            description: 'I looks nice together if you like boring pastels lacking contrast.',
+            colors: ['#00A800', '#0070EC'],
+        },
+        {
+            name: 'Red / Gray',
+            description: 'This one reminds me of lava and ash. I\'m always happy to see this palette.',
+            colors: ['#FF0000', '#A0A0A0'],
+        },
+        {
+            name: 'Purple / Burgundy',
+            description: 'Nice combo, and the only double-dark set.',
+            colors: ['#7B30C7', '#810000'],
+        },
+        {
+            name: 'Blue / Red',
+            description: 'Makes me think of the American flag, except the colors aren\'t dark enough.',
+            colors: ['#0000FF', '#FF0000'],
+        },
+        {
+            name: 'Red / Orange',
+            description: 'Yuck. It makes me think of clowns or fast food logos.',
+            colors: ['#FF0000', '#FFA000'],
+        },
     ];
+    
 
     let currentTheme = 0;
 
-    function updateTheme() {
-        $(".theme-title").text(themes[currentTheme].title);
-        $(".theme-description").text(themes[currentTheme].description);
+    function applyTheme(theme) {
+        $('.theme-name').text(theme.name);
+        $('.theme-description').text(theme.description);
+        $('body').css('color', theme.colors[1]);
+        drawTetrominoesBackground(theme.colors);
     }
 
-    updateTheme();
+    applyTheme(themes[currentTheme]);
 
-    $(document).on("keydown", function (event) {
-        if (event.which === 32) { // Space key
+    $(document).on('keydown', function (e) {
+        if (e.key === ' ') {
+            e.preventDefault();
             currentTheme = (currentTheme + 1) % themes.length;
-            updateTheme();
+            applyTheme(themes[currentTheme]);
         }
     });
 
-    // Play Tetris music
-    const audioElement = document.getElementById("tetris-audio");
-    audioElement.play();
+    // Play the Tetris music on loop
+    const tetrisMusic = document.getElementById('tetris-music');
+    tetrisMusic.play();
+
+
+    function drawTetrominoesBackground(colors) {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+        const blockSize = 25;
+
+        // Tetrominoes shapes
+        const shapes = [
+            [
+                [1, 1, 1],
+                [0, 1, 0],
+            ],
+            [
+                [1, 1, 0],
+                [0, 1, 1],
+            ],
+            [
+                [0, 1, 1],
+                [1, 1, 0],
+            ],
+            [
+                [1, 1],
+                [1, 1],
+            ],
+            [
+                [1, 1, 1, 1],
+            ],
+            [
+                [1, 1, 1],
+                [1, 0, 0],
+            ],
+            [
+                [1, 1, 1],
+                [0, 0, 1],
+            ],
+        ];
+
+        function drawTetromino(x, y, shape, color) {
+            ctx.fillStyle = color;
+            for (let row = 0; row < shape.length; row++) {
+                for (let col = 0; col < shape[row].length; col++) {
+                    if (shape[row][col]) {
+                        ctx.fillRect(x + col * blockSize, y + row * blockSize, blockSize, blockSize);
+                    }
+                }
+            }
+        }
+
+        function generateRandomTetromino() {
+            const shapeIndex = Math.floor(Math.random() * shapes.length);
+            const x = Math.floor(Math.random() * (canvas.width / blockSize)) * blockSize;
+            const y = Math.floor(Math.random() * (canvas.height / blockSize)) * blockSize;
+            const color = colors[Math.floor(Math.random() * colors.length)];
+
+            drawTetromino(x, y, shapes[shapeIndex], color);
+        }
+
+        for (let i = 0; i < 100; i++) {
+            generateRandomTetromino();
+        }
+    }
 });
