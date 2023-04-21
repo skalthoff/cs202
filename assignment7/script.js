4c5e98c478dd5a53b8da2b6fc140cd8f5dafccfc
Soren Althoff <sorenalthoff@gmail.com>
2023-04-21 12:30:41 -0700

    added the jquerry now we keep grinding

--- assignment7/script.js
+++ assignment7/script.js
@@ -1,71 +1,49 @@
-
-
-
-
-// Add your EPIC API key here
 const EPIC_API_KEY = "EhjrQ7GHtgJ9eaYizPgqwY9ow5Y8rGIq3XJgEgbS";
+const peopleContainer = $("#people-container");
 
 // Fetch the latest image from EPIC API and set it as the background
-fetch(
-  `https://api.nasa.gov/EPIC/api/natural/images?api_key=${EPIC_API_KEY}`
-)
-  .then((response) => response.json())
-  .then((data) => {
+$.getJSON(
+  `https://api.nasa.gov/EPIC/api/natural/images?api_key=${EPIC_API_KEY}`,
+  function (data) {
     const latestImage = data[data.length - 1];
     const imageUrl = `https://api.nasa.gov/EPIC/archive/natural/${latestImage.date
       .substr(0, 10)
       .replace(/-/g, "/")}/png/${latestImage.image}.png?api_key=${EPIC_API_KEY}`;
-    document.body.style.backgroundImage = `url(${imageUrl})`;
-  });
+    $("body").css("background-image", `url(${imageUrl})`);
+  }
+);
 
-// The rest of the code remains unchanged...
-const peopleContainer = document.getElementById("people-container");
+// Fetch the data from the People in Space API using jQuery
+$.getJSON("http://api.open-notify.org/astros.json?callback=?", function (data) {
+  // Mock altitude data
+  const mockAltitudeData = data.people.map((person, index) => ({
+    craft: person.craft,
+    altitude: (index + 1) * 10000, // Assuming a difference of 10,000 meters
+  }));
 
-// Fetch the data from the People in Space API
-fetch("https://api.open-notify.org/astros.json")
-  .then((response) => response.json())
-  .then((data) => {
-    // Mock altitude data
-    const mockAltitudeData = data.people.map((person, index) => ({
-      craft: person.craft,
-      altitude: (index + 1) * 10000, // Assuming a difference of 10,000 meters
-    }));
+  displayPeopleInSpace(data, mockAltitudeData);
+});
 
-    displayPeopleInSpace(data, mockAltitudeData);
-  });
+// The rest of the functions remain unchanged...
 
 function displayPeopleInSpace(peopleData, altitudeData) {
-  const groupedPeople = groupPeopleByCraft(peopleData.people);
+  const groupedByCraft = groupPeopleByCraft(peopleData.people);
 
-  for (const craft in groupedPeople) {
-    const craftDiv = document.createElement("div");
-    craftDiv.className = "craft";
+  for (const craft in groupedByCraft) {
+    const craftElement = createCraftElement(craft);
+    const craftPeople = groupedByCraft[craft];
 
-    const craftName = document.createElement("div");
-    craftName.className = "craft-name";
-    craftName.textContent = craft;
-    craftDiv.appendChild(craftName);
-
-    groupedPeople[craft].forEach((person) => {
-      const personDiv = document.createElement("div");
-      personDiv.className = "person";
-
-      const personName = document.createElement("span");
-      personName.textContent = person.name;
-      personDiv.appendChild(personName);
-
-      const personAltitudeData = altitudeData.find(
-        (data) => data.craft === person.craft
-      );
-      const scaledHeight = (personAltitudeData.altitude / 100000) * 100;
-      personDiv.style.paddingTop = `${scaledHeight}px`;
-
-      craftDiv.appendChild(personDiv);
+    craftPeople.forEach((person) => {
+      const altitude = altitudeData.find((data) => data.craft === craft).altitude;
+      const personElement = createPersonElement(person, altitude);
+      craftElement.appendChild(personElement);
     });
 
-    peopleContainer.appendChild(craftDiv);
+    // Use the 'append' method instead of 'appendChild'
+    peopleContainer.append(craftElement);
   }
 }
+
 
 function groupPeopleByCraft(people) {
   return people.reduce((grouped, person) => {
