037c7718f4a84508f0cfc9e06094f9b9fe145167
Soren Althoff <sorenalthoff@gmail.com>
2023-05-10 18:04:40 -0700

    used compas instead

--- assignment7/script.js
+++ assignment7/script.js
@@ -3,21 +3,32 @@ function playNote() {
     const noteMapping = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
   
     if ('geolocation' in navigator) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        let latitude = position.coords.latitude;
-        let longitude = position.coords.longitude;
-  
-        // Combine the latitude, longitude, and dayOfWeek to determine the note
-        let combinedValue = Math.floor(latitude) + Math.floor(longitude) + dayOfWeek;
-        let note = noteMapping[combinedValue % 7];
-        document.getElementById('note-name').innerText = note;
-  
-        // Play the note using the Web Audio API or another library
-      });
+      navigator.geolocation.watchPosition(
+        (position) => {
+          const heading = position.coords.heading;
+          if (heading !== null) {
+            const direction = Math.floor(heading / 45);
+            const note = noteMapping[(dayOfWeek + direction) % 7];
+            document.getElementById('note-name').innerText = note;
+            
+            // Play the note using the Web Audio API or another library
+          } else {
+            alert('Cannot obtain compass heading. Make sure your device has a compass and try again.');
+          }
+        },
+        (error) => {
+          alert('Error obtaining location: ' + error.message);
+        },
+        {
+          enableHighAccuracy: true,
+          timeout: 5000,
+          maximumAge: 0,
+        }
+      );
     } else {
       alert('Geolocation is not supported by your browser.');
     }
   }
   
-  document.getElementById('play-button').addEventListener('click', playNote);
+  document.getElementById('play-note').addEventListener('click', playNote);
   
