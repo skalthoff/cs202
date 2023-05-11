88b92aaea07842e668affb969f6de8b5ff1f6445
Soren Althoff <sorenalthoff@gmail.com>
2023-05-10 22:25:09 -0700

    yesir

--- assignment7/script.js
+++ assignment7/script.js
@@ -31,6 +31,49 @@ function playNote() {
       // Play the note using the Web Audio API or another library
     }
   }
+
+document.getElementById('play-note').addEventListener('click', playNote);
+
+function playSound(note) {
+  const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+  const oscillator = audioContext.createOscillator();
+  const gainNode = audioContext.createGain();
+
+  const noteFrequencies = {
+    'C': 261.63,
+    'D': 293.66,
+    'E': 329.63,
+    'F': 349.23,
+    'G': 392.00,
+    'A': 440.00,
+    'B': 493.88,
+  };
+
+  const duration = document.getElementById('duration').value;
+  const frequencyMultiplier = document.getElementById('frequency').value;
+
+  oscillator.frequency.value = noteFrequencies[note] * frequencyMultiplier;
+  oscillator.type = 'sine'; // Change this to 'square', 'sawtooth', or 'triangle' for different sounds
+
+  gainNode.gain.setValueAtTime(0, audioContext.currentTime);
+  gainNode.gain.linearRampToValueAtTime(1, audioContext.currentTime + 0.01);
+
+  oscillator.connect(gainNode);
+  gainNode.connect(audioContext.destination);
+
+  oscillator.start(audioContext.currentTime);
+  gainNode.gain.exponentialRampToValueAtTime(0.001, audioContext.currentTime + duration / 1000);
+
+  setTimeout(() => {
+    oscillator.stop();
+  }, duration);
+}
+
+function handleDeviceOrientation(event) {
+    let direction = Math.floor(event.webkitCompassHeading / 45);
+    let note = noteMapping[(dayOfWeek + direction) % 7];
+    document.getElementById('note-name').innerText = note;
   
-  document.getElementById('play-note').addEventListener('click', playNote);
+    playSound(note);
+  }
   
