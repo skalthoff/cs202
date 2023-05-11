14c8eaf3c563b0d5a18b02c717dc86d4d011b8e5
Soren Althoff <sorenalthoff@gmail.com>
2023-05-10 22:02:46 -0700

    done?

--- assignment7/script.js
+++ assignment7/script.js
@@ -2,31 +2,33 @@ function playNote() {
     const dayOfWeek = new Date().getDay();
     const noteMapping = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
   
-    if ('geolocation' in navigator) {
-      navigator.geolocation.watchPosition(
-        (position) => {
-          const heading = position.coords.heading;
-          if (heading !== null) {
-            const direction = Math.floor(heading / 45);
-            const note = noteMapping[(dayOfWeek + direction) % 7];
-            document.getElementById('note-name').innerText = note;
-            
-            // Play the note using the Web Audio API or another library
-          } else {
-            alert('Cannot obtain compass heading. Make sure your device has a compass and try again.');
-          }
-        },
-        (error) => {
-          alert('Error obtaining location: ' + error.message);
-        },
-        {
-          enableHighAccuracy: true,
-          timeout: 5000,
-          maximumAge: 0,
-        }
-      );
+    // Check for DeviceOrientationEvent support
+    if ('DeviceOrientationEvent' in window) {
+      // Request permission for iOS 13+
+      if (typeof DeviceOrientationEvent.requestPermission === 'function') {
+        DeviceOrientationEvent.requestPermission()
+          .then(permissionState => {
+            if (permissionState === 'granted') {
+              window.addEventListener('deviceorientation', handleDeviceOrientation);
+            } else {
+              alert('Permission to access device orientation was denied.');
+            }
+          })
+          .catch(console.error);
+      } else {
+        // Listen to the deviceorientation event for older devices
+        window.addEventListener('deviceorientation', handleDeviceOrientation);
+      }
     } else {
-      alert('Geolocation is not supported by your browser.');
+      alert('Device orientation is not supported by your browser.');
+    }
+  
+    function handleDeviceOrientation(event) {
+      let direction = Math.floor(event.webkitCompassHeading / 45);
+      let note = noteMapping[(dayOfWeek + direction) % 7];
+      document.getElementById('note-name').innerText = note;
+  
+      // Play the note using the Web Audio API or another library
     }
   }
   
